a426876cc812e429822a70d9e024ef06b63edb24
Zhaoze Wang <wangzhaoze@outlook.com>
2026-09-03 18:42:37 +0200

    fix: load real contact form handler

--- script.js
+++ script.js
@@ -6,5 +6,5 @@
     if (done) s.onload = done;
     document.head.appendChild(s);
   };
-  load('script-base.js', () => load('career-compact.js'));
+  load('script-base.js', () => load('career-compact.js', () => load('contact-form.js')));
 })();
