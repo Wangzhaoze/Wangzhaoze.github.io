adc2864690aa564928de2bbfa61b1177eb1755a4
Zhaoze Wang <wangzhaoze@outlook.com>
2026-09-03 15:20:33 +0200

    fix: restore stable homepage initialization

--- script.js
+++ script.js
@@ -2,9 +2,9 @@
   const load = (src, done) => {
     const s = document.createElement('script');
     s.src = src;
-    s.onload = done;
     s.defer = true;
+    if (done) s.onload = done;
     document.head.appendChild(s);
   };
-  load('script-base.js', () => load('career-showcase.js'));
+  load('script-base.js', () => load('career-compact.js'));
 })();
